954b2e8a3e747691c33c373f9970f168e07dd13e
Andrea <26469651+AndreaJasper@users.noreply.github.com>
2019-07-02 09:24:44 -0600

    Update page-home.php
    converted instruction section to ACF

--- page-home.php
+++ page-home.php
@@ -46,7 +46,18 @@ $project_feature_body = get_field('project_feature_body');
  $course_instruction_body = get_field('course_instruction_body');
  $course_instruction_video = get_field('course_instruction_video');
 
-
+ //Instructor Section//
+ $instructor_section_title = get_field('instructor_section_title');
+ $instructor_name = get_field('instructor_name');
+ $bio_excerpt = get_field('bio_excerpt');
+ $full_bio = get_field('full_bio');
+ $twiter_username = get_field('twitter_username');
+ $facebook_username = get_field('facebook_username');
+ $google_plus = get_field('google_plus');
+ $num_students = get_field('num_students');
+ $num_reviews = get_field('num_reviews');
+ $num_courses = get_field('num_courses');
+ 
 get_header(); ?>
 
   <!-- HERO BANNER
@@ -228,39 +239,43 @@ get_header(); ?>
                 <div class="col-sm-8 col-md-6">
                     <div class="row">
                         <div class="col-lg-8">
-                            <h2>Your Instructor <small class="text-muted">Brad Hussey</small></h2>
+                            <h2><?php echo $instructor_section_title; ?> <small class="text-muted"><?php echo $instructor_name; ?></small></h2>
                         </div>
                         <div class="div col-lg-4">
-                            <a href="https://twiter.com/bradhussey" target="_blank" class="badge social twitter"><i class="fab fa-twitter"></i></a>
-                            <a href="https://facebook.com/bradhussey" target="_blank" class="badge social facebook"><i class="fab fa-facebook-f"></i></a>
-                            <a href="https://plus.google.com/+BradHussey" target="_blank" class="badge social gplus"><i class="fab fa-google-plus-g"></i></a>
+                            <?php if( !empty($twitter_username) ): ?>        
+                                <a href="https://twiter.com/<?php echo $twitter_username; ?>" target="_blank" class="badge social twitter"><i class="fab fa-twitter"></i></a>
+                                <?php endif; ?>
+                            <?php if( !empty($facebook_username) ): ?>        
+                                <a href="https://facebook.com<?php echo $facebook_username; ?>" target="_blank" class="badge social facebook"><i class="fab fa-facebook-f"></i></a>
+                            <?php endif; ?>
+                            <?php if( !empty($google_plus_username) ): ?>        
+                                <a href="https://plus.google.com<?php echo $google_plus_username; ?>" target="_blank" class="badge social gplus"><i class="fab fa-google-plus-g"></i></a>
+                            <?php endif; ?>                                
                         </div>
                     </div>
-                    <p class="lead">A highly skilled professional, Brad Hussey is a passionate and experienced web designer, developer, blogger and digital entrepreneur.</p>
-                    <p>Hailing from North Of The Wall (Yellowknife, Canada), Brad made the trek to the Wet Coast (Vancouver, Canada) to educate and equip himself with the necessary skills to become a spearhead in his trade of solving problems on the web, crafting design solutions, and speaking in code. </p>
-                    <p>Brad's determination and love for what he does has landed him in some pretty interesting places with some neat people. He's had the privilege of working with, and providing solutions for, numerous businesses, big & small, across the Americas. </p>
-                    <p>Brad builds custom websites, and provides design solutions for a wide-array of clientele at his company, Brightside Studios. He regularly blogs about passive income, living your life to the fullest, and provides premium quality web design tutorials and courses for tens of thousands of amazing people desiring to master their craft.</p>
+                    <p class="lead"><?php echo $bio_excerpt; ?></p>
+                    <?php echo $full_bio; ?>
                     <hr>
                     <h3>The Numbers <small class="text-muted">They Don't Lie</small></h3>
                     <div class="row">
                         <div class="col-xs-4">
                             <div class="num">
                                 <div class="num-content">
-                                    41,000+ <span>students</span>
+                                    <?php echo $num_students; ?>+ <span>students</span>
                                 </div>
                             </div>
                         </div>
                         <div class="col-xs-4">
                             <div class="num">
                                 <div class="num-content">
-                                    568 <span>reviews</span>
+                                <?php echo $num_reviews; ?>+ <span>reviews</span>
                                 </div>
                             </div>
                         </div>
                         <div class="col-xs-4">
                             <div class="num">
                                 <div class="num-content">
-                                    8 <span>courses</span>
+                                <?php echo $num_courses; ?> <span>courses</span>
                                 </div>
                             </div>
                         </div>
